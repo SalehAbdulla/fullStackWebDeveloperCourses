d4bcb3db4f08170e01ea03c5c48b6917f83f70c6
SALEH ABDULLA <nonemask1@gmail.com>
2024-08-11 16:02:20 +0300

    I'm The One

---
+++ JavaScriptProjects/jQuery/index.js
@@ -0,0 +1,33 @@
+// //-----------------
+
+// console.log($("img").attr("src"));
+// $("a").attr("href", "https://www.bing.com");
+
+// //-----------------
+// console.log($("h1").attr("class"))
+// //-----------------
+
+// $("h1").text("I Don't care What other people think about me.")
+// $("button").html("<em>I'm Special</em>")
+
+// //-----------------
+
+// $("h1").click(function(){
+//     $("h1").css("color", "purple");
+// });
+
+
+// $("button").click(function(){
+//     $("h1").css("color", "purple");
+// });
+
+
+// $(document).keypress(function(event){
+//     $('h1').text(event.key)
+// })
+
+
+$('button').click(function(){
+    $('h1').toggle();
+})
+

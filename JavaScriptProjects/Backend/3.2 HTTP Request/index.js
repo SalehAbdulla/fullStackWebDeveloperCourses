a473704311fe4104e19b2b2765ea66fdb77d6c46
SALEH ABDULLA <nonemask1@gmail.com>
2024-08-24 14:23:31 +0300

    I'm The One

--- JavaScriptProjects/Backend/3.2 HTTP Request/index.js
+++ JavaScriptProjects/Backend/3.2 HTTP Request/index.js
@@ -3,22 +3,28 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-
 app.get("/", (req, res)=>{
-    res.send(`<h1>Hello World!</h1>`);
+    res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/about", (req, res)=>{
-    res.send(`<h1>This is me EVERY DAY ALL DAY</h1>`)
-})
+app.post("/register", (req, res)=>{
+    // Do somthing with the data
+    res.sendStatus(201);
+});
 
-app.get("/contact", (req, res)=>{
-    res.send(`<h1>Phone: 38389522 </h1>`)
-})
+app.put("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
 
+app.patch("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
 
+app.delete("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
 
-app.listen(port, ()=>{
+app.listen(port, (req, res) => {
     console.log(`Server is running on port ${port}`);
 });
 

90da396de13a375c38c35332dfbd39643a31d323
java <salehabdullaict@gmail.com>
2025-01-30 12:50:55 +0300

    Session

--- JavaScriptProjects/Backend/9.3+Sessions+and+Cookies/index.js
+++ JavaScriptProjects/Backend/9.3+Sessions+and+Cookies/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
 import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
 
 
 const app = express();
@@ -18,7 +20,8 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 const db = new pg.Client({
   user: "postgres",
@@ -40,6 +43,16 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+
+app.get("/secrets", (req, res)=>{
+  // console.log(req.user);
+  if (req.isAuthenticated()){
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
+  }
+})
+
 
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -73,34 +86,41 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
 
+
+});
+
+
+passport.use(new Strategy(async function verify(username, password, cb){
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
+      username,
     ]);
+
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedHashedPassword = user.password;
-      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+
+      bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
-          console.error("Error comparing passwords:", err);
+          cb(err);
         } else {
           if (result) {
-            res.render("secrets.ejs");
+            cb(null, user);
           } else {
-            res.send("Incorrect Password");
+            cb(null, false);
           }
         }
       });
     } else {
-      res.send("User not found");
+      cb("User not found");
     }
+
   } catch (err) {
-    console.log(err);
+    cb("Query error");
   }
-});
+}))
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

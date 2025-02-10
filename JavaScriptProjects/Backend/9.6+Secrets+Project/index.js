1e971714dda0be58f546128bc2514e257259f58c
java <salehabdullaict@gmail.com>
2025-02-10 17:28:36 +0300

    Project Done

--- JavaScriptProjects/Backend/9.6+Secrets+Project/index.js
+++ JavaScriptProjects/Backend/9.6+Secrets+Project/index.js
@@ -1,87 +1,60 @@
+// Import  packages
 import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
-import bcrypt from "bcrypt";
+import session from "express-session";
 import passport from "passport";
+import bcrypt from "bcrypt";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
-import session from "express-session";
+import pg from "pg";
 import env from "dotenv";
+
+
+// Initialise Enviroment variables
+env.config()
+
+
+// Initialise variables
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
-env.config();
+const db = new pg.Client({
+  user:process.env.PG_USER,
+  host:process.env.PG_HOST,
+  database:process.env.PG_DATABASE,
+  password:process.env.PG_PASSWORD,
+  port:process.env.PG_PORT,
+});
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(bodyParser.urlencoded({ extended: true }));
+db.connect();
+
+
+// Initialise MiddleWares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("public"));
-
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 1000* 60*60*24},
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
-db.connect();
+// Initialise Get routes
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
 });
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.get("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
-
-app.get("/secrets", async (req, res) => {
-  console.log(req.user);
-
-  ////////////////UPDATED GET SECRETS ROUTE/////////////////
-  if (req.isAuthenticated()) {
-    try {
-      const result = await db.query(
-        `SELECT secret FROM users WHERE email = $1`,
-        [req.user.email]
-      );
-      console.log(result);
-      const secret = result.rows[0].secret;
-      if (secret) {
-        res.render("secrets.ejs", { secret: secret });
-      } else {
-        res.render("secrets.ejs", { secret: "No secret added!" });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  } else {
-    res.redirect("/login");
-  }
-});
-
-//TODO: Add a get route for the submit button
 
 app.get("/submit", (req, res) => {
   if (req.isAuthenticated()){
@@ -89,153 +62,142 @@ app.get("/submit", (req, res) => {
   } else {
     res.redirect("/login");
   }
-  
 });
 
-app.post("/submit", async function (req, res) {
-  const submittedSecret = req.body.secret;
-  console.log(req.user);
+app.get("/logout", (req, res)=>{
+  req.logout((err)=>{
+    if (err) console.log(err);
+    res.redirect("/login");
+  });
+});
+
+
+app.post("/submit", async (req, res)=>{
+  const user = req.user;
+  const secret = req.body.secret;
   try {
-    await db.query(`UPDATE users SET secret = $1 WHERE email = $2`, [
-      submittedSecret,
-      req.user.email,
-    ]);
+    await db.query("UPDATE users SET secret = $1 WHERE email = $2;", [secret, user.email]);
     res.redirect("/secrets");
   } catch (err) {
-    console.log(err);
+    res.send(err);
   }
 });
 
-//Think about how the logic should work with authentication.
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+app.get("/secrets", (req, res)=>{
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs", {secret: req.user.secret});
+  } else {
+    res.redirect("/login");
+  }
+});
 
-app.get(
-  "/auth/google/secrets",
-  passport.authenticate("google", {
-    successRedirect: "/secrets",
-    failureRedirect: "/login",
-  })
-);
+app.get("/auth/google", passport.authenticate("google", {scope: ["profile", "email"]}));
+app.get("/auth/google/secrets", passport.authenticate("google", {successRedirect: "/secrets", failureRedirect: "/login"}));
 
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/secrets",
-    failureRedirect: "/login",
-  })
-);
+app.post("/login", passport.authenticate("local", {successRedirect: "/secrets", failureRedirect: "/login"}));
 
-app.post("/register", async (req, res) => {
-  const email = req.body.username;
+
+
+
+
+// Register post request
+
+app.post("/register", async (req, res)=>{
+  const username = req.body.username;
   const password = req.body.password;
 
   try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (checkResult.rows.length > 0) {
-      req.redirect("/login");
-    } else {
-      bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if (err) {
-          console.error("Error hashing password:", err);
-        } else {
-          const result = await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
-            [email, hash]
-          );
-          const user = result.rows[0];
-          req.login(user, (err) => {
-            console.log("success");
-            res.redirect("/secrets");
-          });
-        }
+    const isEmailExist = await db.query("SELECT * FROM users WHERE email = $1", [username]);
+    if (isEmailExist.rows.length === 0){
+      const hashPassword = await bcrypt.hash(password, saltRounds);
+      const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *", [username, hashPassword]);
+      const user = result.rows[0];
+      req.login(user, (err) => {
+        if (err){
+          return res.send("Error Occur, try again later", err);
+        } 
+        res.redirect("/secrets");
       });
+    } else {
+      res.send("Email already Exist, try to login");
     }
-  } catch (err) {
+  } catch (err){
     console.log(err);
+    res.send("Error Occur, try again later.")
   }
 });
 
-//TODO: Create the post route for submit.
-//Handle the submitted data and add it to the database.
 
-passport.use(
-  "local",
-  new Strategy(async function verify(username, password, cb) {
-    try {
-      const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
-        username,
-      ]);
-      if (result.rows.length > 0) {
-        const user = result.rows[0];
-        const storedHashedPassword = user.password;
-        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-          if (err) {
-            console.error("Error comparing passwords:", err);
-            return cb(err);
-          } else {
-            if (valid) {
-              return cb(null, user);
-            } else {
-              return cb(null, false);
-            }
-          }
-        });
+
+passport.use("local", new Strategy(async function verify(username, password, cb){
+
+  try {
+      const isEmailExist = await db.query("SELECT * FROM users WHERE LOWER(email) = $1", [username.toLowerCase()]);
+      if (isEmailExist.rows.length > 0){
+        const user = isEmailExist.rows[0];
+        const isPassCorrect = await bcrypt.compare(password, user.password);
+        if (isPassCorrect){
+          return cb(null, user);
+        } else {
+          return cb(null, false);
+        }
       } else {
         return cb("User not found");
       }
-    } catch (err) {
-      console.log(err);
-    }
-  })
-);
+  } catch (err){
+    console.log(err);
+    return cb(err);
+  }
+}));
 
-passport.use(
-  "google",
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-    async (accessToken, refreshToken, profile, cb) => {
-      try {
-        console.log(profile);
-        const result = await db.query("SELECT * FROM users WHERE email = $1", [
-          profile.email,
-        ]);
-        if (result.rows.length === 0) {
-          const newUser = await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
-            [profile.email, "google"]
-          );
-          return cb(null, newUser.rows[0]);
-        } else {
-          return cb(null, result.rows[0]);
-        }
-      } catch (err) {
-        return cb(err);
-      }
+
+passport.use("google", new GoogleStrategy({
+
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+
+}, async (accessToken, refreshToken, profile, cb)=>{
+
+  try {
+    const isEmailExist = await db.query("SELECT * FROM users WHERE email = $1", [profile.email]);
+    if (isEmailExist.rows.length == 0){
+      const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *;", [profile.email, profile.id]);
+      const user = result.rows[0];
+      return cb(null, user);
+    } else {
+      return cb(null, isEmailExist.rows[0]);
     }
-  )
-);
-passport.serializeUser((user, cb) => {
-  cb(null, user);
+
+  } catch (err) {
+    return cb(err);
+  }
+
+}));
+
+
+passport.serializeUser((user, cb)=>{
+  return cb(null, user.id);
 });
 
-passport.deserializeUser((user, cb) => {
-  cb(null, user);
+passport.deserializeUser(async (id, cb)=>{
+
+  try {
+    const isIdExist = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+    if (isIdExist.rows.length > 0){
+      return cb(null, isIdExist.rows[0]);
+    } else {
+      return cb(null, false);
+    }
+  } catch (err){
+    return cb(err);
+  }
+
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`);
 });

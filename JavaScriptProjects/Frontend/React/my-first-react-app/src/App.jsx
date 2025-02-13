60a4a940a15508d7cc3b43e51c8eb4fb12de3da3
java <salehabdullaict@gmail.com>
2025-02-13 18:34:07 +0300

    my-first-react-app

---
+++ JavaScriptProjects/Frontend/React/my-first-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState();
+  return <div className="card"><h2>{title}</h2>
+    <button onClick={()=> setHasLiked(!hasLiked)} >
+      {hasLiked ? "❤️": "🤍"}
+    </button>
+  </div>;
+
+};
+
+const App = () => {
+  
+  return (
+    <div className="card-container">
+
+      <Card title="Star Wars" rating={5} isCool={true} actors={[{name : 'Actors'}]}/>
+      <Card title="Avatar" />
+      <Card title="The lion King" />
+
+    </div >
+  );
+};
+
+export default App;

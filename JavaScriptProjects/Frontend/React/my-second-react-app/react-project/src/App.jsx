816cf2491f2a01f5591bc8a067dbc3aa714a750a
java <salehabdullaict@gmail.com>
2025-02-14 15:15:56 +0300

    Session Two

--- JavaScriptProjects/Frontend/React/my-second-react-app/react-project/src/App.jsx
+++ JavaScriptProjects/Frontend/React/my-second-react-app/react-project/src/App.jsx
@@ -6,14 +6,25 @@ const fName = "Saleh";
 const lName = "Abdulla";
 const date = new Date().getFullYear();
 
+const imgOne = "https://picsum.photos/200/";
 
 const App = ()=> {
   return (
     <div>
-      <h1>Created by {fName + " " + lName}</h1>
+      <div className="heading" spellCheck="true">
+        <h1>React heading</h1>
+
+        <div className="food-images">
+          <img alt="randomimage" className="photos" src={imgOne}></img>
+          <img alt="randomimage" className="photos" src={imgOne}></img>  
+          <img alt="randomimage" className="photos" src={imgOne}></img>
+        </div>
+
+      </div>
+      <h1 id="name">Created by {fName + " " + lName}</h1>
       <p>Copyright {date}</p>
     </div>
   )
-}
+};
 
 export default App;

4928ef67c5e057f0fcf4ed1dc1820dc40fd00129
java <salehabdullaict@gmail.com>
2025-02-15 21:33:19 +0300

    Practice

---
+++ JavaScriptProjects/Frontend/React/my-five-react-app/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MovieCard from "./components/movieCard.jsx"
+// component
+const App = () => {
+  return (
+    <>
+      <div>
+      <MovieCard movie="HI"/>
+      </div>
+    </>
+  )
+};
+
+
+
+export default App

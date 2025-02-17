f7596efb08f5264164415f9e49504ecf7cf3ae08
java <salehabdullaict@gmail.com>
2025-02-17 11:01:54 +0300

    I'm The One

---
+++ JavaScriptProjects/Frontend/React/my-seventh-react-app/front-end/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './components/MovieCard'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+
+
+
+
+const App = () => {
+
+  const movieNumber = 2;
+  
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ JavaScriptProjects/Frontend/React/my-seventh-react-app/front-end/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import MovieCard from "../components/MovieCard"
+
+
+const Home = () => {
+
+    const movies = [{
+        id: 1,
+        title: "pk",
+        url: "http://localhost:3000",
+        release_date: "1998",
+    },
+    {
+        id: 2,
+        title: "3idiots",
+        url: "http://localhost:3000",
+        release_date: "2015",
+    },
+    {
+        id: 3,
+        title: "The Matrix",
+        url: "http://localhost:3000",
+        release_date: "2015",
+    },
+    {
+        id: 4,
+        title: "Terminator",
+        url: "http://localhost:3000",
+        release_date: "2015",
+    },
+    ]
+
+    return (
+        <div>
+           {movies.map((movie)=> <MovieCard movie={movie}/>)}
+        </div>
+    )
+
+}
+
+export default Home

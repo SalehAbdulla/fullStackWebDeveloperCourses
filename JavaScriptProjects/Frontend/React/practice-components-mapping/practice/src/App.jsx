5302c6d66e37cf60ebcd5392698c6fdf7ecb59c4
java <salehabdullaict@gmail.com>
2025-02-26 11:00:14 +0300

    components practice + using different kind of loop in JS

---
+++ JavaScriptProjects/Frontend/React/practice-components-mapping/practice/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Entry from './components/Entry';
+import emojipedia from '../emojipedia';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {emojipedia.map(param => (
+          <Entry
+            key={param.id}
+            emoji={param.emoji}
+            name={param.name}
+            meaning={param.meaning}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+
+export default App
---
+++ JavaScriptProjects/Frontend/React/practice-components-mapping/practice/src/components/Entry.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Entry = (param) => {
+    return (
+        <div>
+            <h1>{param.id}</h1>
+            <h1>{param.emoji}</h1>
+            <h1>{param.name}</h1>
+            <p>{param.meaning}</p>
+        </div>
+    )
+}
+
+export default Entry;

42adb33cd9f6f9f6e3762c4499b3693de4f234ca
java <salehabdullaict@gmail.com>
2025-03-02 18:46:12 +0300

    I'm MEE

---
+++ JavaScriptProjects/Frontend/React/destructuring/constructring/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import animals from './data'
+import cars from './practice'
+
+
+const [honda, tesla] = cars;
+const {speedStats : {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed : teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <table>
+          <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+          </tr>
+          <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+          </tr>
+          <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  )
+}
+
+
+export default App
---
+++ JavaScriptProjects/Frontend/React/destructuring/constructring/src/data.jsx
@@ -0,0 +1,7 @@
+const animals = [
+    { name: "cat", sound: "meow", feedingRequiremnt: {food: 2, water: 3} },
+    { name: "dog", sound: "woof" }
+];
+
+
+export default animals;

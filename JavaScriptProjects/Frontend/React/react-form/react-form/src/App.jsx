1bf907397e79b779a875e13b2dc6dd5706284dac
java <salehabdullaict@gmail.com>
2025-03-23 06:29:37 +0300

    I'm MEEE

--- JavaScriptProjects/Frontend/React/react-form/react-form/src/App.jsx
+++ JavaScriptProjects/Frontend/React/react-form/react-form/src/App.jsx
@@ -6,31 +6,38 @@ import React from 'react';
 function App() {
 
 
-  const [fName, setFName] = useState();
-  const [lName, setLName] = useState();
+  const [fullName, setFullName] = useState({firstName: "", lastName: ""});
 
-  function handleFName(event){
-    setFName(event.target.value);
+  function handleOneChange(event) {
+
+    const {name, value} = event.target;
+
+    setFullName(prevValue => {
+      console.log(prevValue);
+
+      if (name === "firstName") {
+        return ({firstName: value, lastName: prevValue.lastName});
+      } else if (name === "lastName") {
+        return ({firstName: prevValue.firstName, lastName: value});
+      }
+
+    })
+
   }
 
-  function handleLName(event) {
-    setLName(event.target.value);
-  }
 
- 
   return (
     <>
-
       <div className='container'>
+
         <form>
-          <h1>Hello {fName} {lName}</h1>
-          <input onChange={handleFName} type='text' name='fName' placeholder='First Name'/>
-          <input onChange={handleLName} type='text' name='lName' placeholder='Last Name' />
+          <h1>Hello {fullName.firstName} {fullName.lastName}</h1>
+          <input onChange={handleOneChange} name='firstName' value={fullName.firstName} placeholder='First Name'/>
+          <input onChange={handleOneChange} name='lastName' value={fullName.lastName} placeholder='Last Name' />
           <button>Submit</button>
         </form>
-
+      
       </div>
-
     </>
   )
 }

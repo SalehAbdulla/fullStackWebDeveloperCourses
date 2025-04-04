ff3cef8b44eddde036c7946f3a017e877a06e4a4
java <salehabdullaict@gmail.com>
2025-04-04 14:39:00 +0300

    I'm The One

---
+++ JavaScriptProjects/Frontend/nextJsCourse/my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1 className="text-center text-lg text-cyan-400 mt-2 w-full font-mono font-extrabold text-[50px]">Hello World</h1>
+    </>
+  );
+};
+
+export default HomePage;

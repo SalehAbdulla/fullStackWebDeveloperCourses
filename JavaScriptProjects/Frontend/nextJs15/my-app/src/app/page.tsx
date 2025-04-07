1a34cb33dbba6dbbd9febdf7f8af9bf0a83e9ca5
java <salehabdullaict@gmail.com>
2025-04-07 18:14:58 +0300

    I'm The best

---
+++ JavaScriptProjects/Frontend/nextJs15/my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+
+  console.log("Who am I .?");
+
+
+  return (
+    <div>
+      <h1 className="text-3xl">Hello this me learning Next.js15, This is a test of HMR hot modulue replacement, it works like nodemon and other folks</h1>
+    </div>
+  );
+}

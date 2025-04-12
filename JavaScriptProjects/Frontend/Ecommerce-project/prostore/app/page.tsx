58d05a0ab4da5f1a860cb94699d43fae6d43e3b6
java <salehabdullaict@gmail.com>
2025-04-12 16:17:48 +0300

    I'm The One

---
+++ JavaScriptProjects/Frontend/Ecommerce-project/prostore/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-content-center space-y-6">
+        <h1>HomePage</h1>
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;

96f3000e0235c1fb23be5670ec854bd7bcf0a2f4
java <salehabdullaict@gmail.com>
2025-04-18 23:25:18 +0300

    I'm The One

--- JavaScriptProjects/mern-stack/client/src/context/AppContext.jsx
+++ JavaScriptProjects/mern-stack/client/src/context/AppContext.jsx
@@ -1,18 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
 
     const navigate = useNavigate();
-    const [user, setUser] = userState(null);
 
-    const value = {}
+    const [user, setUser] = userState(null);
+    const [isSeller, setIsSeller] = useState(null);
+
+    const value = {navigate, user, setUser, setIsSeller, isSeller}
+
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
 }
 
 export const useAppContext = () => {
-    return useContext(App);
+    return useContext(AppContextProvider);
 }
+

918482ec10d1bb0e8c66ea2268fe983936bd0548
java <salehabdullaict@gmail.com>
2025-05-20 15:55:55 +0300

    I'm the One

---
+++ JavaScriptProjects/GreenStore/client/src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+import {useNavigate} from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const value = {};
+    
+    return <AppContext.Provider value={value} >
+        {children}
+    </AppContext.Provider>
+};
+
+export const useAppContext = () => {
+  useContext(AppContext);
+};

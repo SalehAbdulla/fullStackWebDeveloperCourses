536ceeb11351a9708a72b145e34a3dab32bccc14
java <salehabdullaict@gmail.com>
2025-05-25 21:48:14 +0300

    react day 4 to-do-list

--- ReactBroCodeCourse/my-react-app/src/components/ToDoList/ToDoList.tsx
+++ ReactBroCodeCourse/my-react-app/src/components/ToDoList/ToDoList.tsx
@@ -11,20 +11,48 @@ export const ToDoList = () => {
     "Workout",
     "Zeyarat Aashoraa",
   ]);
+
   const [newTask, setNewTask] = useState("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewTask(event.currentTarget.value);
   };
 
-  const handleAddTask = (newTask:string) => {
-    setTasks(tasks => [...tasks, newTask]);
+  const handleAddTask = (newTask: string) => {
+    setTasks((tasks) => [...tasks, newTask]);
     setNewTask("");
   };
   const handleRemoveTask = (index: number) => {
-    setTasks(tasks.filter((_,i) => index != i));
+    setTasks(tasks.filter((_, i) => index != i));
   };
-  const moveTaskUp = (index: number) => {};
+
+  const moveTaskUp = (index: number) => {
+    if (index > 0) {
+      const updatedArray = [...tasks];
+      [updatedArray[index], updatedArray[index - 1]] = [
+        updatedArray[index - 1],
+        updatedArray[index],
+      ];
+      setTasks(updatedArray);
+    }
+  };
+
+  const moveTaskDown = (index: number) => {
+    if (index < tasks.length - 1) {
+      const updatedArray = [...tasks];
+      [updatedArray[index], updatedArray[index + 1]] = [
+        updatedArray[index + 1],
+        updatedArray[index],
+      ];
+      setTasks(updatedArray);
+    }
+  };
+
+  const handleKeyDown = <T extends HTMLElement>(event: React.KeyboardEvent<T>) => {
+    if (event.key === "Enter") {
+      handleAddTask(newTask);
+    }
+  };
 
   return (
     <div className="to-do-list">
@@ -35,15 +63,16 @@ export const ToDoList = () => {
           placeholder="Enter a task"
           value={newTask}
           onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
         />
-        <button onClick={()=> handleAddTask(newTask)} className="add-button">
+        <button onClick={() => handleAddTask(newTask)} className="add-button">
           Add
         </button>
       </div>
 
       <ol>
         {tasks.map((task, index) => (
-          <li>
+          <li key={index}>
             <span className="text">{task}</span>
             <div className="all-btn">
               <button
@@ -52,12 +81,11 @@ export const ToDoList = () => {
               >
                 Delete
               </button>
-              <button
-                className="move-button"
-              >
+              <button onClick={() => moveTaskUp(index)} className="move-button">
                 ☝️
               </button>
               <button
+                onClick={() => moveTaskDown(index)}
                 className="move-button"
               >
                 👇
@@ -66,7 +94,6 @@ export const ToDoList = () => {
           </li>
         ))}
       </ol>
-
     </div>
   );
 };
